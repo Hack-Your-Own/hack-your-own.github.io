84e830ab1656fb30e64ec2af9de050f640eda0fe
Hack-Your-Own <lzhang.0693@gmail.com>
2020-07-04 21:01:54 +0000

    Update from https://github.com/Hack-Your-Own/V2-Website/commit/b1ff5975bea048bd83eb6ef0d9c9344157140a0d)

---
+++ precache-manifest.3c9181471d10139e388bc65b633a67db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b2be5ee1ccbee79ad0a96effe427df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87901010c4cc907e916e",
+    "url": "/static/css/main.b44c3e0b.chunk.css"
+  },
+  {
+    "revision": "0df6cbc481dd7a37dad3",
+    "url": "/static/js/2.87c055f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.87c055f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87901010c4cc907e916e",
+    "url": "/static/js/main.528e3a51.chunk.js"
+  },
+  {
+    "revision": "985b9523fa7178415d1f",
+    "url": "/static/js/runtime-main.670d09c7.js"
+  },
+  {
+    "revision": "ea48169107ac6abba855ca2c59cc3db5",
+    "url": "/static/media/logo.ea481691.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/V2-Website/precache-manifest.3ad664a071db7a368328dd80b0e7d0f5.js"
+  "/precache-manifest.3c9181471d10139e388bc65b633a67db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/V2-Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
